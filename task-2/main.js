4a20c6f7e956f92820d3938dd1699a6a659670d6
KananFarmanli <105041431+KananFarmanli@users.noreply.github.com>
2022-07-22 15:51:04 +0300

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,48 @@
-const prompt = require('prompt');
-prompt.start();
+let num = 22
+function odd  (x) {
+    if (x==2 || x ==3 || x == 5 || x ==7){
+        console.log('yes')
+    }else if(x%2==0 || x%3==0 || x%5==0 || x%7==0) {
+        console.log('no')
+    }else {console.log('yes')}
+}
+odd(num)
 
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+
+// ПОСМОТРИ ВТОРУЮ ФУНКЦЦИЮ КАК Я РЕШИЛ И СКАЖИ ХОРОШЕЕЕ ЛИ ЭТО РЕШЕНИЕ? КАК РАЗ ЕСЛИ ПОСМОТРИШЬ Я ПОЙМУ ТЫ ПРОВЕРЯЕШЬ МОЮ ДОМАШКУ ИЛИ НЕТ АХАХАХ!
+
+// НАХОЖУ ВСЕ ДЕЛИТЕЛИ ЧИСЛА А ПОТОМ МНОЖИТЕЛИ ПРОВЕРЬ!!!!!!!
+
+// let regem = 34
+
+
+// function election (num) {
+// let k = []
+// let m = []
+// let count=0
+// for (let i = 0; i<1; i++) {
+//         for (let j = 0; j <= num; j++) { 
+//             num%j==0 && j? k.push(j) : -1  
+//         }
+        
+//         for (let j = 0; j < k.length; j++) {
+//             function odd  (x) {
+//                 if (x==2 || x ==3 || x == 5 || x ==7){
+//                     m.push(x)
+//                 }else if(x%2==0 || x%3==0 || x%5==0 || x%7==0) {
+//                     console.log('no')
+//                 }else {m.push(x)}
+//             }
+//             odd(k[j])
+            
+            
+//         }
+
+
+
+// }
+// console.log(`все делители ${k}`)
+// console.log(`все множители ${m}`)
+// }
+// election(regem)
